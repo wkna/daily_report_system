1dd1b791ca0923b8562eca942f03fada5ee8953b
wakana.morikubo <m.wakana1208@gmail.com>
2021-10-04 15:24:28 +0900

    従業員destroyアクションの作成

--- src/main/java/actions/ActionBase.java
+++ src/main/java/actions/ActionBase.java
@@ -15,6 +15,10 @@ import constants.AttributeConst;
 import constants.ForwardConst;
 import constants.PropertyConst;
 
+/**
+ * 各Actionクラスの親クラス。共通処理を行う。
+ *
+ */
 public abstract class ActionBase {
     protected ServletContext context;
     protected HttpServletRequest request;
@@ -54,15 +58,20 @@ public abstract class ActionBase {
         Method commandMethod;
         try {
 
+            //パラメータからcommandを取得
             String command = request.getParameter(ForwardConst.CMD.getValue());
 
+            //ommandに該当するメソッドを実行する
+            //(例: action=Employee command=show の場合 EmployeeActionクラスのshow()メソッドを実行する)
             commandMethod = this.getClass().getDeclaredMethod(command, new Class[0]);
-            commandMethod.invoke(this, new Object[0]);
+            commandMethod.invoke(this, new Object[0]); //メソッドに渡す引数はなし
 
         } catch (NoSuchMethodException | SecurityException | IllegalAccessException | IllegalArgumentException
                 | InvocationTargetException | NullPointerException e) {
 
+            //発生した例外をコンソールに表示
             e.printStackTrace();
+            //commandの値が不正で実行できない場合エラー画面を呼び出し
             forward(ForwardConst.FW_ERR_UNKNOWN);
         }
 
@@ -76,9 +85,11 @@ public abstract class ActionBase {
      */
     protected void forward(ForwardConst target) throws ServletException, IOException {
 
+        //jspファイルの相対パスを作成
         String forward = String.format("/WEB-INF/views/%s.jsp", target.getValue());
         RequestDispatcher dispatcher = request.getRequestDispatcher(forward);
 
+        //jspファイルの呼び出し
         dispatcher.forward(request, response);
 
     }
@@ -93,11 +104,13 @@ public abstract class ActionBase {
     protected void redirect(ForwardConst action, ForwardConst command)
             throws ServletException, IOException {
 
+        //URLを構築
         String redirectUrl = request.getContextPath() + "/?action=" + action.getValue();
         if (command != null) {
             redirectUrl = redirectUrl + "&command=" + command.getValue();
         }
 
+        //URLへリダイレクト
         response.sendRedirect(redirectUrl);
 
     }
@@ -110,10 +123,12 @@ public abstract class ActionBase {
      */
     protected boolean checkToken() throws ServletException, IOException {
 
+        //パラメータからtokenの値を取得
         String _token = getRequestParam(AttributeConst.TOKEN);
 
         if (_token == null || !(_token.equals(getTokenId()))) {
 
+            //tokenが設定されていない、またはセッションIDと一致しない場合はエラー画面を表示
             forward(ForwardConst.FW_ERR_UNKNOWN);
 
             return false;
--- src/main/java/actions/EmployeeAction.java
+++ src/main/java/actions/EmployeeAction.java
@@ -176,5 +176,20 @@ public class EmployeeAction extends ActionBase {
             }
         }
     }
+    /**
+     * @throws ServletException
+     * @throws IOException
+     */
+    public void destroy() throws ServletException, IOException {
+
+        if (checkToken()) {
+
+            service.destroy(toNumber(getRequestParam(AttributeConst.EMP_ID)));
+
+            putSessionScope(AttributeConst.FLUSH, MessageConst.I_DELETED.getMessage());
+
+            redirect(ForwardConst.ACT_EMP, ForwardConst.CMD_INDEX);
+        }
+    }
 
 }
