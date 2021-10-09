d329994c2dcbdd07343536a34aba6d661475c616
wakana.morikubo <m.wakana1208@gmail.com>
2021-10-09 12:19:54 +0900

    ログインアクションの作成

--- src/main/java/constants/AttributeConst.java
+++ src/main/java/constants/AttributeConst.java
@@ -33,7 +33,8 @@ public enum AttributeConst {
     REP_ID("id"),
     REP_DATE("report_date"),
     REP_TITLE("title"),
-    REP_CONTENT("content");
+    REP_CONTENT("content"),
+    LOGIN_EMP("text");
 
     private final String text;
     private final Integer i;
