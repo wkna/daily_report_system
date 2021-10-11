7e91cab55079f48582bc7161f1710c764003109a
wakana.morikubo <m.wakana1208@gmail.com>
2021-10-11 14:57:01 +0900

    EncodingFilter作成

--- src/main/java/filters/EncodingFilter.java
+++ src/main/java/filters/EncodingFilter.java
@@ -9,30 +9,15 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
-
-/**
- * Servlet Filter implementation class EncodingFilter
- */
 @WebFilter("/*")
 public class EncodingFilter implements Filter {
-
-    /**
-     * Default constructor.
-     */
     public EncodingFilter() {
 
     }
 
-    /**
-     * @see Filter#destroy()
-     */
     public void destroy() {
 
     }
-
-    /**
-     * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
-     */
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         request.setCharacterEncoding("UTF-8");
         response.setCharacterEncoding("UTF-8");
@@ -40,9 +25,6 @@ public class EncodingFilter implements Filter {
         chain.doFilter(request, response);
     }
 
-    /**
-     * @see Filter#init(FilterConfig)
-     */
     public void init(FilterConfig fConfig) throws ServletException {
 
     }
