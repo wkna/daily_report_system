7a950c8fabd2137e85a0393dc1a11707142fbec3
wakana.morikubo <m.wakana1208@gmail.com>
2021-10-11 19:29:08 +0900

    日報newアクションの作成

---
+++ src/main/java/actions/ReportAction.java
@@ -0,0 +1,57 @@
+package actions;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.List;
+
+import javax.servlet.ServletException;
+
+import actions.views.ReportView;
+import constants.AttributeConst;
+import constants.ForwardConst;
+import constants.JpaConst;
+import services.ReportService;
+
+public class ReportAction extends ActionBase {
+    private ReportService service;
+
+    @Override
+    public void process() throws ServletException, IOException {
+
+        service = new ReportService();
+
+        invoke();
+        service.close();
+    }
+
+    public void index() throws ServletException, IOException {
+
+        int page = getPage();
+        List<ReportView> reports = service.getAllPerPage(page);
+
+        long reportsCount = service.countAll();
+
+        putRequestScope(AttributeConst.REPORTS, reports);
+        putRequestScope(AttributeConst.REP_COUNT, reportsCount);
+        putRequestScope(AttributeConst.PAGE, page);
+        putRequestScope(AttributeConst.MAX_ROW, JpaConst.ROW_PER_PAGE);
+
+        String flush = getSessionScope(AttributeConst.FLUSH);
+        if (flush != null) {
+            putRequestScope(AttributeConst.FLUSH, flush);
+            removeSessionScope(AttributeConst.FLUSH);
+        }
+
+        forward(ForwardConst.FW_REP_INDEX);
+    }
+
+    public void entryNew() throws ServletException, IOException {
+
+        putRequestScope(AttributeConst.TOKEN, getTokenId());
+        ReportView rv = new ReportView();
+        rv.setReportDate(LocalDate.now());
+        putRequestScope(AttributeConst.REPORT, rv);
+
+        forward(ForwardConst.FW_REP_NEW);
+
+    }
+}
